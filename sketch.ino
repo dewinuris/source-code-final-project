a330263e7bc295b9b3f7b0ba557a7377a1828eba
dewinuris <133210781+dewinuris@users.noreply.github.com>
2024-01-07 21:46:44 +0700

    Add files via upload

---
+++ sketch.ino
@@ -0,0 +1,20 @@
+void setup() {
+  pinMode(2, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(2, HIGH);
+  delay(10000);
+  digitalWrite(2, LOW);
+
+  digitalWrite(4, HIGH);
+  delay(1500);
+  digitalWrite(4, LOW);
+
+  digitalWrite(5, HIGH);
+  delay(10000);
+  digitalWrite(5, LOW);
+}
+
